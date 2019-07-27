5eae6b6853b415c9ab34d22ee4caff544e446de6
David Bingham <david.r.bingham@gmail.com>
2019-07-27 22:01:04 +0100

    Test

---
+++ gateway.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "products", url: "http://localhost:9001/graphql" }
+  ]
+});
+
+(async () => {
+  const { schema, executor } = await gateway.load();
+
+  const server = new ApolloServer({ schema, executor });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
